79d3d47dfc918766c419a7ef55bad45e776d383f
Matthew Liu <matt@ledgerinvesting.com>
2019-02-05 15:44:02 -0500

    adds package exports

---
+++ xy-chart/src/index.js
@@ -0,0 +1,9 @@
+import PointSeriesSpring from 'PointSeriesSpring';
+import LineSeriesSpring from 'LineSeriesSpring';
+import AreaSeriesSpring from 'AreaSeriesSpring';
+
+export {
+  PointSeriesSpring as PointSeriesSpring,
+  LineSeriesSpring as LineSeriesSpring,
+  AreaSeriesSpring as AreaSeriesSpring,
+};
