3db6c1c0e0a180e0994d7b219f21cae4d6719524
Matthew Liu <matt@ledgerinvesting.com>
2019-02-07 17:04:19 -0500

    Removes package files from gh-pages repo

--- xy-chart/src/index.js
+++
@@ -1,9 +0,0 @@
-import PointSeriesSpring from './PointSeriesSpring/index.jsx';
-import LineSeriesSpring from './LineSeriesSpring/index.jsx';
-import AreaSeriesSpring from './AreaSeriesSpring/index.jsx';
-
-export {
-  PointSeriesSpring as PointSeriesSpring,
-  LineSeriesSpring as LineSeriesSpring,
-  AreaSeriesSpring as AreaSeriesSpring,
-};
